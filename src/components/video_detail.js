0e43dc9a9266dbedf9589ec6016e37970b25ff38
Jack Gutteridge <jack@kingbrick.co.uk>
2016-11-22 13:53:22 +0000

    Progress up to lecture 30

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,3 +1,25 @@
- /**
- * Created by Jack Gutteridge on 22/11/2016.
- */
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+
+    if (!video) {
+        return <div>Loading..</div>;
+    }
+
+    return (
+        <div className="video-detail col-md-8">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe
+                    className="embed-responsive-item"
+                    src={`https://www.youtube.com/embed/${video.id.videoId}`}
+                />
+            </div>
+            <div className="details">
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoDetail;
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,3 +1,23 @@
-/**
- * Created by Jack Gutteridge on 22/11/2016.
- */
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+
+    const videoItems = props.videos.map((video) => {
+        return (
+            <VideoListItem
+                onVideoSelect={props.onVideoSelect}
+                key={video.etag}
+                video={video}
+            />
+        );
+    });
+
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    );
+};
+
+export default VideoList;
