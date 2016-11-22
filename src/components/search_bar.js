2b9e954bcc5bb9d543c56e2af4d53a998a7f0a55
Jack Gutteridge <jack@kingbrick.co.uk>
2016-11-22 14:19:13 +0000

    Progress up to lecture 32

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,12 +8,17 @@ class SearchBar extends Component {
         this.state = { term: '' };
     }
 
+    onInputChanged(term) {
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
+    }
+
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     value={this.state.term}
-                    onChange={event => this.setState({ term: event.target.value })} />
+                    onChange={event => this.onInputChanged(event.target.value)} />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -19,10 +19,15 @@ class App extends Component {
             selectedVideo: null
         };
 
+        this.videoSearch('Doom');
+    }
+
+    videoSearch(term) {
+
         YTSearch(
             {
                 key: API_KEY,
-                term: 'surfboards'
+                term: term
             },
             (videos) => {
                 this.setState({
@@ -36,7 +41,9 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar
+                    onSearchTermChange={term => this.videoSearch(term)}
+                />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
