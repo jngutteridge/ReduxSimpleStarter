768b99cd404278b148cc4cc598a7a66c6dd7ca38
Jack Gutteridge <jack@kingbrick.co.uk>
2016-11-22 13:05:07 +0000

    Progress up to lecture 22

---
+++ src/components/video_detail.js
@@ -0,0 +1,3 @@
+ /**
+ * Created by Jack Gutteridge on 22/11/2016.
+ */
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCsJeth3PniRBkW1Xx6TUVTnMhod40ggIs';
+
+YTSearch(
+    {
+        key: API_KEY,
+        term: 'surfboards'
+    },
+    function(data) {
+        console.log(data);
+    }
+);
 
 // Create new component to produce HTML
 const App = () => {
-    return <div>Hello</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 };
 
 // Inject component into DOM
