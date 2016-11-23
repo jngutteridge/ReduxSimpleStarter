9dfc5a3b7f79f6f9a99411f532b73bb438c2c89e
Jack Gutteridge <jack@kingbrick.co.uk>
2016-11-23 09:56:30 +0000

    Minor tidy up

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-        <App />
-    </Provider>
-    , document.querySelector('.container'));
+    (
+        <Provider store={createStoreWithMiddleware(reducers)}>
+            <App />
+        </Provider>
+    ),
+    document.querySelector('.container')
+);
