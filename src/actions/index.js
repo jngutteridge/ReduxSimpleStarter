631e7c8c16afa51139790296b96d6e548cdeb3db
Jack Gutteridge <jack@kingbrick.co.uk>
2016-11-23 14:26:24 +0000

    Progress up to lecture 60

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '4b3bd6db190128a907f09ee437bdc775';
-const ENTRY_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ENTRY_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
