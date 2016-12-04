fe804a56e9745ddd54eb874f4bc3e72847d797ad
Jack Gutteridge <jack@kingbrick.co.uk>
2016-12-04 17:50:05 +0000

    Progress up to lecture 91

---
+++ src/components/posts-new.js
@@ -0,0 +1,78 @@
+import React, { Component, PropTypes } from 'react';
+import { reduxForm } from 'redux-form';
+import { Link } from 'react-router';
+
+import { createPost } from './../actions/index';
+
+class PostsNew extends Component {
+
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    onSubmit(props) {
+        this.props.createPost(props)
+            .then(() => {
+                this.context.router.push('/');
+            });
+    }
+
+    render() {
+
+        const { fields: { title, categories, content }, handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <h3>Create a New Post</h3>
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title} />
+                    <div className="form-control-label">
+                        {title.touched ? title.error : ''}
+                    </div>
+                </div>
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories} />
+                    <div className="form-control-label">
+                        {categories.touched ? categories.error : ''}
+                    </div>
+                </div>
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
+                    <label>Content</label>
+                    <textarea className="form-control" name="" {...content} />
+                    <div className="form-control-label">
+                        {content.touched ? content.error : ''}
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Create New Post</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+            </form>
+        );
+    }
+}
+
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Enter a title';
+    }
+
+    if (!values.categories) {
+        errors.categories = 'Enter categories';
+    }
+
+    if (!values.content) {
+        errors.content = 'Enter content';
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content'],
+    validate
+}, null, { createPost })(PostsNew);
